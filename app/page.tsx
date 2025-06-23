ec6f08aba181f669b8aa829afe87aff65c71b92f
oCaioAug <ocaioaug.dev@protonmail.com>
2025-06-22 21:55:42 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 // import Image from "next/image";
 
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-  return <h1 className="bg-red mb-5 text-red-500">Hello, World!</h1>
+  return <Button>Test</Button>
 }

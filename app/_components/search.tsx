c33d72efb6a439d997d6b999c8afea670254f94f
oCaioAug <ocaioaug.dev@protonmail.com>
2025-06-30 12:32:22 -0300

    feat: ensure user can search for barbershops

---
+++ app/_components/search.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { SearchIcon } from 'lucide-react'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+import { useRouter } from 'next/navigation'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { Form, FormControl, FormField, FormItem, FormMessage } from './ui/form'
+
+const formSchema = z.object({
+  search: z.string().trim().min(1, {
+    message: 'O campo de busca não pode estar vazio.',
+  }),
+})
+
+const Search = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: '',
+    },
+  })
+
+  const router = useRouter()
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershops?search=${data.search}`)
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="flex gap-2">
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Faça sua busca aqui..." {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          ></FormField>
+
+          <Button type="submit">
+            <SearchIcon />
+          </Button>
+        </form>
+      </Form>
+    </>
+  )
+}
+
+export default Search
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import { SearchIcon } from 'lucide-react'
 import Header from './_components/header'
 import { Button } from './_components/ui/button'
-import { Input } from './_components/ui/input'
 import Image from 'next/image'
 import { db } from './_lib/prisma'
 import BarbershopItem from './_components/barbershop-item'
 import { quickSearchOption } from './_constants/search'
 import BookingItem from './_components/booking-item'
+import Search from './_components/search'
 
 export default async function Home() {
   const barbershops = await db.barbershop.findMany({})
@@ -27,11 +26,8 @@ export default async function Home() {
         <p>Terça-feira, 24 de Junho.</p>
 
         {/* BUSCA */}
-        <div className="mt-6 flex items-center gap-2">
-          <Input placeholder="Faça sua busca aqui..." />
-          <Button>
-            <SearchIcon />
-          </Button>
+        <div className="mt-6">
+          <Search />
         </div>
 
         {/* BUSCA RÁPIDA */}
